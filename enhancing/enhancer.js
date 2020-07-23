963f7a8c5393df37178fef8bf3ab8d446ae3d1e3
Daniel Johnson <danielaugustusjohnson@gmail.com>
2020-07-23 11:28:23 -0400

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,79 @@
 module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+	success,
+	fail,
+	repair,
+	get,
 };
 
 function success(item) {
-  return { ...item };
+	if (item.enhancement === 20) {
+		return "You're already level 20!";
+	} else {
+		return {
+			...item,
+			enhancement: item.enhancement + 1,
+		};
+	}
 }
 
 function fail(item) {
-  return { ...item };
+	let newItem = { ...item };
+
+	switch (true) {
+		case item.enhancement < 15:
+			newItem = {
+				...newItem,
+				durability: item.durability - 5,
+      };
+      break;
+
+		case item.enhancement >= 15:
+			newItem = {
+				...newItem,
+				durability: item.durability - 10,
+      };
+      break;
+  }
+  
+  if (item.enhancement > 16) {
+    newItem = {
+      ...newItem,
+      enhancement: item.enhancement - 1
+    }
+	}
+
+	return {
+		...newItem,
+	};
 }
 
 function repair(item) {
-  return { ...item };
+	return {
+    ...item,
+    durability: 100
+  };
 }
 
 function get(item) {
-  return { ...item };
+	return { ...item };
 }
+
+//Example usage
+
+let axe = {
+  name: 'axe',
+  enhancement: 20,
+  durability: 50
+}
+
+console.log(
+	'Success Output:',
+	success(axe)
+);
+
+console.log(
+	'Fail Output: ',
+	fail(axe)
+);
+
+console.log('Repair output:', repair(axe))
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,71 @@
 const enhancer = require('./enhancer.js');
+
 // test away!
+
+test('Success output', () => {
+	//Setup
+
+	let axe = {
+		name: 'axe',
+		enhancement: 20,
+		durability: 50,
+	};
+
+	let expectedValue = "You're already level 20!";
+
+	//Execute code
+
+	let actual = enhancer.success(axe);
+
+	//Assert results
+
+	expect(actual).toBe(expectedValue);
+});
+
+test('Repair output', () => {
+	//Setup
+
+	let axe = {
+		name: 'axe',
+		enhancement: 20,
+		durability: 50,
+	};
+
+	let expectedValue = {
+		name: 'axe',
+		enhancement: 20,
+		durability: 100,
+	};
+
+	//Execute code
+
+	let actual = enhancer.repair(axe);
+
+	//Assert results
+
+	expect(actual).toStrictEqual(expectedValue);
+});
+
+test('Fail output', () => {
+	//Setup
+
+	let axe = {
+		name: 'axe',
+		enhancement: 20,
+		durability: 50,
+	};
+
+	let expectedValue = {
+		name: 'axe',
+		enhancement: 19,
+		durability: 40,
+	};
+
+	//Execute code
+
+	let actual = enhancer.fail(axe);
+
+    //Assert results
+
+	expect(actual).toStrictEqual(expectedValue);
+});
